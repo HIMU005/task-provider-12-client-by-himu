fafddef0c7dc25b54396a99b4a85f003e990d62e
HIMU005 <hashanuzzaman99@gamil.com>
2024-06-04 01:25:37 +0600

    tailwind and daisy setup and basic route done. Now time for a sound sleep. tomorrow is a busy day

---
+++ src/Layouts/Main.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+    return (
+        <div>
+            <h2 className="text-2xl italic">I am main layout</h2>
+            <Outlet />
+        </div>
+    );
+};
+
+export default Main;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,31 @@
 import {
     createBrowserRouter,
 } from "react-router-dom";
+import Main from "../Layouts/Main";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Authentication/Login";
+import Register from "../pages/Authentication/Register";
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <h2>Home</h2>
+        element: <Main />,
+        errorElement: <h3 className="text-5xl">404</h3>,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            }
+        ],
+    },
+    {
+        path: '/login',
+        element: <Login />,
+    },
+    {
+        path: "/sign-up",
+        element: <Register />
     }
+
+
 ])
