f7a9c7b47be12c35aa9f7a9259709f4fd194f4ad
HIMU005 <hashanuzzaman99@gamil.com>
2024-06-08 09:41:10 +0600

    improve in usesecure

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,33 +1,35 @@
 import axios from "axios";
 import useAuth from "./useAuth";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
 
 
 export const axiosSecure = axios.create({
     baseURL: "http://localhost:5000",
-    // withCredentials: true,
 })
 
 const useAxiosSecure = () => {
     const { logOut } = useAuth();
     const navigate = useNavigate();
 
-    useEffect(() => {
-        axiosSecure.interceptors.response.use(
-            res => {
-                return res
-            },
-            async error => {
-                console.log('error tracked in the interceptor', error.response)
-                if (error.response.status === 401 || error.response.status === 403) {
-                    await logOut()
-                    navigate('/login')
-                }
-                return Promise.reject(error)
-            }
-        )
-    }, [logOut, navigate])
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('access-token')
+        config.headers.Authorization = token;
+        return config;
+    }, function (error) {
+        return Promise.reject(error)
+    })
+
+    axiosSecure.interceptors.response.use(function (response) {
+        return response;
+    }, async (error) => {
+        const status = error.response.status;
+        if (status === 401 || status === 403) {
+            await logOut();
+            navigate('/login')
+        }
+        return Promise.reject(error)
+    })
+
 
     return axiosSecure
 }
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -48,11 +48,13 @@ const AuthProvider = ({ children }) => {
                     .then(res => {
                         if (res.data.token) {
                             localStorage.setItem('access-token', res.data.token);
+                            setLoading(false)
                         }
                     })
             } else {
                 // TODO: remove the token if the token store in the client side 
                 localStorage.removeItem('access-token')
+                setLoading(false)
             }
 
             // console.log("current user", currentUser);
