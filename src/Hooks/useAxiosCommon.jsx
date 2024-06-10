ba2137840cfd87386f22356049f1f823533f1b81
HIMU005 <hashanuzzaman99@gamil.com>
2024-06-10 21:59:15 +0600

    final deploy

--- src/Hooks/useAxiosCommon.jsx
+++ src/Hooks/useAxiosCommon.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const axiosCommon = axios.create({
-    baseURL: 'http://localhost:5000',
-    // baseURL: 'https://task-management-himu005.vercel.app',
+    // baseURL: 'http://localhost:5000',
+    baseURL: 'https://task-management-himu005.vercel.app',
 })
 
 const useAxiosCommon = () => {
