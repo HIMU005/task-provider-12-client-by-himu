cf9e04678e4dcf429cec9b5d4d5e995a2942faef
HIMU005 <hashanuzzaman99@gmail.com>
2025-05-19 10:21:03 +0600

    micro update

--- src/pages/DashBoard/Home/WorkerHome/TaskDetails.jsx
+++ src/pages/DashBoard/Home/WorkerHome/TaskDetails.jsx
@@ -223,7 +223,7 @@ const TaskDetails = () => {
             <div className="mt-4">
               <input
                 type="submit"
-                value="Add Task"
+                value="Submit Task"
                 className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 cursor-pointer"
               />
             </div>
